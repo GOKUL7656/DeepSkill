57f6fcb28b7c869a06876bdd62b06eddfc51a98e
Gokul K <kgokul1526@gmail.com>
2025-07-11 14:22:52 +0530

    spring handson-4 to display country via web browser is completed

---
+++ Week 4/spring-handson-4/spring-learn/src/main/java/com/cognizant/springlearn/controller/CountryController.java
@@ -0,0 +1,30 @@
+package com.cognizant.springlearn.controller;
+
+import com.cognizant.springlearn.Country;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CountryController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CountryController.class);
+
+    @RequestMapping("/country")
+    public Country getCountryIndia() {
+        LOGGER.info("START - getCountryIndia() method");
+
+        try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("country.xml")) {
+            Country country = (Country) context.getBean("country", Country.class);
+            LOGGER.debug("Country loaded: {}", country);
+            LOGGER.info("END - getCountryIndia() method");
+            return country;
+        } catch (Exception e) {
+            LOGGER.error("Error loading country bean: {}", e.getMessage());
+            return null;
+        }
+    }
+}
