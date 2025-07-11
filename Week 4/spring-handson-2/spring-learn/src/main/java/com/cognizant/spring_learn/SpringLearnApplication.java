72678b9f3ddb4b3b83dbcab77f4f49585ed3926c
Gokul K <kgokul1526@gmail.com>
2025-07-11 13:22:53 +0530

    spring-handson-2 completed

---
+++ Week 4/spring-handson-2/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
@@ -0,0 +1,34 @@
+package com.cognizant.spring_learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@SpringBootApplication
+public class SpringLearnApplication {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SpringLearnApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLearnApplication.class, args);
+        LOGGER.info("SpringLearnApplication main() method started and application is running.");
+
+        displayCountry();
+    }
+
+    private static void displayCountry() {
+        LOGGER.info("Starting displayCountry() method.");
+
+        try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("country.xml")) {
+
+            Country country = (Country) context.getBean("country", Country.class);
+
+            LOGGER.debug("Country : {}", country.toString());
+        } 
+        
+        LOGGER.info("Ending displayCountry() method.");
+    }
+}
